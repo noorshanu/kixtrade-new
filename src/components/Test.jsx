b4af31e9d4270b1d97facfce9c60dc215a2f0d7f
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 00:43:44 +0530

    Update Test.jsx

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -7,7 +7,7 @@ function Test() {
         <div>
           <img src="images/test-mg.png" alt="" className="h-[600px]" />
         </div>
-        <div className="bg-grad p-[2px] m-2 sm:mr-28  ">
+        <div className="bg-grad p-[2px] m-2 sm:mr-[9.5rem]  ">
           <div className="bg-sec h-full">
             <h1 className=" text-4xl font-extrabold py-3">TEST DURATION</h1>
             <h3 className="text-xl font-bold py-2">KiX Trade-athon starts on Friday 3rd November.</h3>
