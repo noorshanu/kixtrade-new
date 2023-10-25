9edde552ff7d8f8868ded88203047e48207c9334
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 00:25:25 +0530

    for

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
-import Footer from "./components/Footer"
-import Hero from "./components/Hero"
-import Rewards from "./components/Rewards"
-import About from "./components/About"
-import Getstarted from "./components/Getstarted"
-import Test from "./components/Test"
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Rewards from "./components/Rewards";
+import About from "./components/About";
+import Getstarted from "./components/Getstarted";
+import Test from "./components/Test";
 
 function App() {
-
-
   return (
     <>
-     <Hero/>
-     <div className="py-10">
-      <About/>
+      <Hero />
+      <div className="py-10">
+        <About />
+      </div>
+      <div className="py-10">
+        <Rewards />
+      </div>
 
-     </div>
-     <div className="py-10">
-    <Rewards/>
+      <div className="py-10">
+        <Getstarted />
+      </div>
+      <Test />
 
-     </div>
-
-     <div className="py-10">
-   <Getstarted/>
-
-     </div>
-     <Test/>
-
-     <div className="py-10">
-   <Footer/>
-
-     </div>
-
+      <div className="py-10">
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
