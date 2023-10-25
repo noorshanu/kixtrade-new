3a0309fc5ace4c286c4e09a9ebb0bf8b42d8d569
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 00:17:54 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
 <section>
     <div className='container-wrapper'>
         <div className=' flex flex-col sm:flex-row justify-between '>
-            <img src="images/logo-about.png" alt="" />
+            <img src="images/logo-about.png" alt="" className='h-[55px] sm:h-auto w-[55px] sm:w-auto sm:mx-0 mx-auto mb-6 sm:mb-0' />
 
             <div className=' flex justify-evenly gap-5 text-center items-center'>
                 <a href="/" className=' border border-[#fff] hover:text-[#82067e] rounded-full py-4 px-4'><BsTwitter className=' text-xl' /></a>
