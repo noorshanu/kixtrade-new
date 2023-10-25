5c67b7f51d9f9c72e7aa743583eeac83f6506207
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-25 23:34:52 +0530

    new

---
+++ src/components/Getstarted.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Getstarted() {
+  return (
+<section>
+    <div className='container-wrapper'>
+        <div className=' text-center'>
+            <h1 className=' text-4xl font-extrabold '>THERE ARE 2 WAYS TO GET STARTED</h1>
+
+        </div>
+
+        <div className='flex justify-between gap-10 flex-col sm:flex-row'>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+  )
+}
+
+export default Getstarted
--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Rewards() {
   return (
- <section className=' text-center mt-32'>
-    <div>
-    <h1 className=" text-4xl font-extrabold text pt-10">TRADE-ATHON REWARDS</h1>
-    <p className=" text-[22px] font-normal sm:max-w-5xl w-[90%] sm:w-full mx-auto mt-7 ">
-        Qualified entrants who connect their crypto-wallet to the KiX Test platform will be able to claim 1000 FreeKiX tokens and then play for a share of 1,000,000 $KiX tokens
-        <br /> in the KiX game.
-</p>
-    </div>
- </section>
-  )
+    <section className=" text-center mt-32">
+      <div>
+        <h1 className=" text-4xl font-extrabold text pt-10">
+          TRADE-ATHON REWARDS
+        </h1>
+        <p className=" text-[22px] font-normal sm:max-w-5xl w-[90%] sm:w-full mx-auto mt-7 ">
+          Qualified entrants who connect their crypto-wallet to the KiX Test
+          platform will be able to claim 1000 FreeKiX tokens and then play for a
+          share of 1,000,000 $KiX tokens
+          <br /> in the KiX game.
+        </p>
+
+        <img src="images/100k2.png" alt="" className=" mx-auto -mt-12" />
+        <div className=""> 
+        <img src="images/bg-main2.png" alt=""  className="w-full -mt-[12rem]" />
+
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Rewards
+export default Rewards;
