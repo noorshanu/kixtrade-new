d9f5634c223a74a1b6e6edeb4486fd34912c1c67
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 01:16:55 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <section className='hero'>
-        <div className=' relative'>
-            <img src="images/hero-up.png" alt="" className=' mx-auto' />
-<img src="images/player.png" alt="" className=' object-cover mx-auto  center-div z-0' />
-<img src="images/100k.png" alt=""  className='mx-auto relative z-20'/>
-<img src="images/black-b.png" alt="" className='w-full absolute z-10 bottom-0' />
-        </div>
-
+    <section className="hero">
+      <div className=" relative">
+        <img src="images/freeplay.png" alt="" className=" absolute top-[30%] left-[10%]" />
+        <img src="images/open.png" alt="" className=" absolute top-[20%] right-[10%]" />
+        <img src="images/hero-up.png" alt="" className=" mx-auto" />
+        <img
+          src="images/player.png"
+          alt=""
+          className=" object-cover mx-auto  center-div z-0"
+        />
+        <img src="images/100k.png" alt="" className="mx-auto relative z-20" />
+        <img
+          src="images/black-b.png"
+          alt=""
+          className="w-full absolute z-10 bottom-0"
+        />
+      </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
