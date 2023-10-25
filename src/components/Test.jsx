825e19a37d7850899aea8f98b7a086b4cf65f3f3
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 03:07:17 +0530

    Update Test.jsx

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -5,11 +5,11 @@ function Test() {
     <section className="test-bg relative  w-full  h-full">
       <div className="container-wrapper">
       
-      <div className="flex   gap-5 items-center flex-col sm:flex-row h-full w-full">
+      <div className="flex gap-5 items-center flex-col sm:flex-row h-full w-full">
         <div className="w-full  h-full">
          
         </div>
-        <div className="bg-grad3 w-auto lg:w-auto  p-[2px] m-2 ml-11   ">
+        <div className="bg-grad3 w-auto lg:w-auto  p-[2px] m-2 sm:ml-11 mt   ">
           <div className="bg-sec h-full">
             <h1 className=" text-4xl font-extrabold py-3">TEST DURATION</h1>
             <h3 className="text-xl font-bold py-2">KiX Trade-athon starts on Friday 3rd November.</h3>
