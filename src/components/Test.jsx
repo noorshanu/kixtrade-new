db38eb896a3d77247969642a9f7c94946032e6a1
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 00:35:46 +0530

    Update Test.jsx

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -9,20 +9,20 @@ function Test() {
         </div>
         <div className="bg-grad p-[2px] m-2">
           <div className="bg-sec h-full">
-            <h1>TEST DURATION</h1>
+            <h1 className=" text-4xl font-extrabold py-3">TEST DURATION</h1>
             <h3>KiX Trade-athon starts on Friday 3rd November.</h3>
-            <p>
+            <p className=" text-3xl font-semibold">
               Entrants into the Trade-athon can join at any time before or after
               that date and will qualify for 1000 $FreeKiX after completing
               registration.
             </p>
-            <p>
+            <p className=" text-3xl font-semibold">
               Entrants can build their chances of winning a share of the prize
               fund by completing tasks set on the KiX Zealy platform which opens
               on Wednesday 24th October.
             </p>
 
-            <p>Terms & Conditions</p>
+            <p className="text-[#FFC200] text-xl">Terms & Conditions</p>
           </div>
         </div>
       </div>
