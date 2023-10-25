65b4c5f8bc804e2a93cd6480433d1b848a9d8be9
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 02:42:56 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ function Hero() {
   return (
     <section className="hero relative">
       <div className="container-wrapper relative">
-        <img src="images/freeplay.png" alt="" className=" absolute top-[24%] left-[5%]" />
-        <img src="images/open.png" alt="" className=" absolute top-[20%] right-[2%]" />
+        <img src="images/freeplay.png" alt="" className=" absolute top-[20%] sm:top-[24%] left-[12%]  sm:left-[5%] h-[133px] sm:h-auto " />
+        <img src="images/open.png" alt="" className=" absolute top-[20%] sm:right-[2%] right-[12%] h-[133px] sm:h-auto  " />
         <img src="images/hero-up.png" alt="" className=" mx-auto" />
         <img
           src="images/player.png"
