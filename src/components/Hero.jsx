7582a5d0997a5436247a7b5d23d5968b28c5a407
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 02:37:36 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className="hero">
+    <section className="hero relative">
       <div className="container-wrapper relative">
         <img src="images/freeplay.png" alt="" className=" absolute top-[24%] left-[5%]" />
         <img src="images/open.png" alt="" className=" absolute top-[20%] right-[2%]" />
@@ -13,12 +13,13 @@ function Hero() {
           className=" object-cover mx-auto  center-div z-0"
         />
         <img src="images/100k.png" alt="" className="mx-auto relative z-20" />
-        <img
+       
+      </div>
+      <img
           src="images/black-b.png"
           alt=""
           className="w-full absolute z-10 bottom-0"
         />
-      </div>
     </section>
   );
 }
