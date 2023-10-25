209fffc557cd6a7b8c66705784e948ac5de7e01d
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-26 00:28:13 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Rewards from "./components/Rewards";
-import About from "./components/About";
+
 import Getstarted from "./components/Getstarted";
 import Test from "./components/Test";
+import About from "./components/about";
 
 function App() {
   return (
     <>
       <Hero />
       <div className="py-10">
-        <About />
+       <About/>
       </div>
       <div className="py-10">
         <Rewards />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
   return (
-    <section className=" mt-20">
+    <section className=" mt-7 sm:mt-20">
       <div>
         <img src="images/logo-about.png" alt="" className="mx-auto" />
       </div>
