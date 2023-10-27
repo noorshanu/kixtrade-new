66132df05e0c8df5962acb7af71a42fd5b0f845b
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-27 13:58:17 +0530

    new

--- src/components/NewAbout.jsx
+++ src/components/NewAbout.jsx
@@ -14,12 +14,9 @@ function NewAbout() {
         <p className=" text-base sm:text-[22px] font-normal sm:max-w-4xl w-[90%] sm:w-full mx-auto mt-7 lh-1_4">
           <strong>KiX Trade-athon</strong> is starting soon and YOU can claim
           1,000 FreeKiX game tokens NOW so you’re ready to{" "}
-          <strong>play for a share of 1 MILLION real $KiX</strong>
-          on the{" "}
-          <strong>
+          <strong>play for a share of 1 MILLION real $KiX</strong>  on the <strong>
             World’s Most Advanced Fantasy Football Trading Exchange
-          </strong>{" "}
-          .
+          </strong>.
         </p>
       </div>
       <div className="mt-4 sm:mt-16 text-center relative z-10">
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -15,7 +15,7 @@ function Test() {
             <div class="w-full mx-auto table-1">
               <table class="w-full">
                 <tbody>
-                  <tr className=" py-4 my-4 flex flex-col flex-no wrap sm:table-row">
+                  <tr className=" py-4 my-4 flex flex-col flex-no wrap sm:table-row border-b border-[#00000042]">
                     <td class="py-2 px-4 w-[350px]">
                       {" "}
                       <h1 className="text-[22px] font-normal text-center">
@@ -34,7 +34,7 @@ function Test() {
                       </div>
                     </td>
                   </tr>
-                  <tr className="flex flex-col flex-no wrap sm:table-row">
+                  <tr className="flex flex-col flex-no wrap sm:table-row border-b border-[#00000042]">
                     <td class="py-2 px-4">
                       <h1 className="text-[22px] font-normal text-center">
                         Wed 8 November
@@ -49,7 +49,7 @@ function Test() {
                     </td>
                   </tr>
 
-                  <tr className="flex flex-col flex-no wrap sm:table-row">
+                  <tr className="flex flex-col flex-no wrap sm:table-row border-b border-[#00000042]">
                     <td class="py-2 px-4">
                     <h1 className="text-[22px] font-normal text-center">Fri 10 November</h1>
                     </td>
@@ -66,9 +66,12 @@ function Test() {
                 </tbody>
               </table>
             </div>
+            <a href="https://docs.google.com/document/d/1GKZVvKESnomDOAVn-w6a6SbojloBdX6ZduWteLmIbuQ/edit">
             <p className="text-[#FFC200] text-xl text-center">
               Terms & Conditions
             </p>
+            </a>
+           
           </div>
         </div>
       </div>
