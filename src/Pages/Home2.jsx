14011ef74d1398cd61f9762167c8e31e05d320a4
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-28 12:44:13 +0530

    new

--- src/Pages/Home2.jsx
+++ src/Pages/Home2.jsx
@@ -6,13 +6,23 @@ import Getstarted from '../components/Getstarted'
 import Test from '../components/Test'
 import Footbal from '../components/Footbal'
 import Footer from '../components/Footer'
-import { useParams } from 'react-router-dom';
+import { Helmet } from "react-helmet";
 import Disclaimer from '../components/v1/Disclaimer'
 
 function Home2() {
   
   return (
     <>
+     <Helmet>
+    
+<script>
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','ognyd');
+</script>
+
+      </Helmet>
     <Hero />
        <div className=" py-4 sm:py-2">
        
